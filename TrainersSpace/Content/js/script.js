26d34182ee008472a82de7ee032c8527e6155829
Tatu Tamminen <ttamminen@gmail.com>
2012-07-03 20:25:20 +0300

    KO bindings for adding client

--- TrainersSpace/Content/js/script.js
+++ TrainersSpace/Content/js/script.js
@@ -8,9 +8,22 @@
     var ViewModel = function (clients) {
         var self = this;
 
+        self.clientName = ko.observable("");
+
         self.clients = ko.observableArray(ko.utils.arrayMap(clients, function (client) {
             return new Client(client.name);
         }));
+
+        self.validName = ko.computed(function () {
+            if (self.clientName().length > 0)
+                return true;
+
+            return false;
+        });
+
+        self.addClient = function () { 
+            self.clients.push(new Client(self.clientName())); 
+        };
     };
 
     // check local storage for todos
