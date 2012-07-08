2f8eef36bfd5ff91d4d5d2416c0ca1db98ac0806
Tatu Tamminen <ttamminen@gmail.com>
2012-07-08 15:35:39 +0300

    Body composition model added

---
+++ TrainersSpace/Models/BodyComposition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainersSpace.Models
+{
+    public class BodyComposition
+    {
+        public decimal Weight { get; set; }
+        public decimal BodyFat { get; set; }
+        public decimal SkeletalMuscle { get; set; }
+        public decimal BodyMassIndex { get; set; }
+        public uint VisceralFat { get; set; }
+        public uint RestingMetabolism { get; set; }
+    }
+}
